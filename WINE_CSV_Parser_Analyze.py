709f9bcb295e7f5b13211f0b5349aafdfbc4486d
mikeditri <mikeditri@hotmail.com>
2019-02-14 16:28:44 -0500

    completed reach assignment to calculate mean and standard deviation on wine data

---
+++ WINE_CSV_Parser_Analyze.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import os
+import numpy as np
+
+myfilename = "wine.data.txt"
+
+with open(myfilename, 'r') as file_handle:
+    mylist = []
+    for line in file_handle.readlines():
+        line_clean = line.replace('   ', ' ').replace('  ', ' ')
+        line_clean = line_clean.strip()
+        values = line_clean.split(',')
+        #print(values)
+        linelist = [int,float,float,float,float,int,float,float,float,float,float,float,float,int]
+        newlist = [t(x) for t,x in zip(linelist,values)]
+        mylist += [newlist]
+#print(mylist[0])
+#print("")#spacer
+rotated_list = [[mylist[jdx][idx]
+                for jdx, row in enumerate(mylist)]
+                for idx, column in enumerate(mylist[0])]
+
+#print(rotated_list[0])
+
+rotated_array = np.array(rotated_list)
+mean_list = []
+sd_list = []
+
+for x in rotated_array:
+    mean_list.append(np.mean(x))
+    sd_list.append(np.std(x))
+
+print("")
+print("List of Means:")
+print("")
+print(mean_list)
+
+print("")
+print("List of Standard Deviations:")
+print("")
+print(sd_list)
+
+print("")
+print('finished!')
+
+
+
+        
